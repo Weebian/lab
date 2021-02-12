bf0328548778193277e8e368d5836808d946cbbd
Hoang Bui <hoangxuanbui6572@gmail.com>
2021-02-11 21:15:12 -0500

    travis test

---
+++ src/test/java/hbui/lab/AppTest.java
@@ -0,0 +1,24 @@
+package hbui.lab;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class AppTest {
+    @Autowired
+    private MockMvc mock;
+
+    @Test
+    public void checkStatus() throws Exception {
+        this.mock.perform(get("/")).andDo(print()).andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/hbui/lab/ControllerTest.java
@@ -0,0 +1,18 @@
+package hbui.lab;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ControllerTest {
+    @Autowired
+    private AddressController controller;
+
+    @Test
+    public void contextLoads() throws Exception{
+        assertThat(controller).isNotNull();
+    }
+
+}
