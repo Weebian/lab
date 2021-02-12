2a848bec81c27fc95067a57b01a8e1388af055e2
Hoang Bui <hoangxuanbui6572@gmail.com>
2021-02-12 10:40:07 -0500

    travis test

--- src/test/java/hbui/lab/AppTest.java
+++ src/test/java/hbui/lab/AppTest.java
@@ -1,9 +1,13 @@
 package hbui.lab;
 
+import static java.util.function.Predicate.not;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +23,13 @@ public class AppTest {
 
     @Test
     public void checkStatus() throws Exception {
-        this.mock.perform(get("/")).andDo(print()).andExpect(status().isOk());
+        this.mock.perform(get("/")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("\"bud\" : {")));
     }
+
+    @Test
+    public void testAddressBookForm() throws Exception {
+        this.mock.perform(get("/addressbook/1")).andExpect(status().isOk())
+                .andExpect(content().string(containsString("Name")));
+    }
+
 }
